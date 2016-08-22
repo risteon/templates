d975914ad087dd7e28c1c2691464dcd766ac0db2
Christoph Rist <rist.christoph@gmail.com>
2016-08-22 23:25:59 +0200

    Add empty python3 script

---
+++ python/python3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import argparse
+
+__author__ = "Author"
+__copyright__ = "Copyright 2016, Author"
+__license__ = "TODO"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="template")
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
+
